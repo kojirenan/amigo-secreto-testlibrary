fba043a0dad5bde9586823ee6001da3aafcf74dc
kojirenan <kj.renan@icloud.com>
2023-07-09 19:18:20 -0300

    add tailwind e estilos dos componentes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        lg: '2px 2px 2px 1px black',
+      },
+    },
+  },
+  plugins: [],
+};
